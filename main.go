ee9b595664fe681cb823b48a4dee9fe116097b88
Enrico Brosque Palma <enricob.palma@gmail.com>
2025-09-28 16:17:48 -0300

    feat(): declares HTTP methods for requests and adds new post method

--- main.go
+++ main.go
@@ -26,11 +26,16 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Snippetbox - Create"))
 }
 
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Save a new snippet"))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/snippet/view/{snippetId}", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{snippetId}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
 	log.Print("Starting server on :4000")
 
